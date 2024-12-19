304f84b65d340c726c76b5d3d432b852172f2e08
lukaszles <lukaszles53@gmail.com>
2024-12-19 23:29:43 +0100

    Implementacja elementu diagonalnego

--- src/gauss.c
+++ src/gauss.c
@@ -3,20 +3,38 @@
 #include <stdlib.h>
 #include <math.h>
 
-/**
- * Zwraca 0 - elimnacja zakonczona sukcesem
- * Zwraca 1 - macierz osobliwa - dzielenie przez 0
- */
+void swap_rows(Matrix *mat, Matrix *b, int r1, int r2) {
+    for (int j = 0; j < mat->c; ++j) {
+        double temp = mat->data[r1][j];
+        mat->data[r1][j] = mat->data[r2][j];
+        mat->data[r2][j] = temp;
+    }
+    double temp = b->data[r1][0];
+    b->data[r1][0] = b->data[r2][0];
+    b->data[r2][0] = temp;
+}
+
 int eliminate(Matrix *mat, Matrix *b) {
-    if (mat->r != mat->c || mat->r!= b->r || b->c != 1) {
+    if (mat->r != mat->c || mat->r != b->r || b->c != 1) {
         return 1; // Niezgodne wymiary macierzy
     }
-    
+
     int n = mat->r;
 
     for (int k = 0; k < n; ++k) {
-        if (fabs(mat->data[k][k]) == 0) {
-            return 1; // Dzielenie przez zero (macierz osobliwa)
+        int max_row = k;
+        for (int i = k + 1; i < n; ++i) {
+            if (fabs(mat->data[i][k]) > fabs(mat->data[max_row][k])) {
+                max_row = i;
+            }
+        }
+
+        if (fabs(mat->data[max_row][k]) == 0) {
+            return 1; // Dzielenie przez zero
+        }
+
+        if (max_row != k) {
+            swap_rows(mat, b, k, max_row);
         }
 
         for (int i = k + 1; i < n; ++i) {
@@ -27,5 +45,6 @@ int eliminate(Matrix *mat, Matrix *b) {
             b->data[i][0] -= factor * b->data[k][0];
         }
     }
+
     return 0; // Eliminacja zakończona sukcesem
 }
